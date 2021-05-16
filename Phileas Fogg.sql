93c63e3a0da5714bbec90d8fb399bc224d8ee960
finn6663 <81816857+finn6663@users.noreply.github.com>
2021-05-17 03:33:34 +0800

    Delete Phileas Fogg.sql

--- Phileas Fogg.sql
+++
@@ -1,8 +0,0 @@
-  
-SELECT DISTINCT model,  AVG(DATE_PART('day',scheduled_arrival - scheduled_departure)*24 + DATE_PART('hour',scheduled_arrival - scheduled_departure)) AS travel_time 
-            FROM flights
-            LEFT JOIN Aircrafts 
-            USING(aircraft_code)
-            GROUP BY model
-            ORDER BY travel_time DESC
-            LIMIT 3
